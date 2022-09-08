4e6fb3677732b17a4ccce86d384c1bfe1a5aac26
fogelo <fogelo@yandex.ru>
2022-09-08 14:33:36 +0300

    some changes

--- sandbox2.js
+++ sandbox2.js
@@ -1,1 +1,27 @@
-console.log(window)
+const button = document.querySelector('.button')
+const div = document.querySelector('.div')
+
+
+// const handler = (e) => {
+//     console.log(this)
+//     console.log(e.currentTarget)
+// }
+
+function handlerButton(e) {
+    console.log('button')
+    console.log(e.currentTarget)
+    console.log(e.target)
+    console.log(this)
+}
+
+function handlerDiv(e) {
+    console.log('div')
+    console.log(e.currentTarget)
+    console.log(e.target)
+    console.log(this)
+}
+
+button.addEventListener('click', handlerButton)
+div.addEventListener('click', handlerDiv)
+
+
