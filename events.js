4a2cf1d83cc0b15ebfcf3d8b8de6fd8f41481545
fogelo <fogelo@yandex.ru>
2022-09-09 17:15:43 +0300

    some changes2

---
+++ events.js
@@ -0,0 +1,13 @@
+// Здесь будет шпаргалка по событиям, погружение и всплытие, делегирование
+// свойство target и currentTarget
+// preventDefault, stopPropagation
+// какие бывают события и что с помощью них можно сделать
+// свойство relatedTarget для mouseover/out, mouseenter/leave
+
+
+
+//drag and drop события
+//keydown и keyup
+
+
+//все это можно почитать на learn js
--- sandbox2.js
+++ sandbox2.js
@@ -11,17 +11,20 @@ function handlerButton(e) {
     console.log('button')
     console.log(e.currentTarget)
     console.log(e.target)
+    console.log(e.relatedTarget)
     console.log(this)
+    window.open('https://javascript.info/')
 }
 
 function handlerDiv(e) {
     console.log('div')
     console.log(e.currentTarget)
     console.log(e.target)
+    console.log(e.relatedTarget)
     console.log(this)
 }
 
-button.addEventListener('click', handlerButton)
-div.addEventListener('click', handlerDiv)
+button.addEventListener('pointerdown', handlerButton)
+div.addEventListener('pointerdown', handlerDiv)
 
 
