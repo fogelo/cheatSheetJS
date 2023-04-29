a0b83dbfbf530d917616a5299c780e838e99f490
fogelo <fogelo@yandex.ru>
2023-04-29 15:55:12 +0300

    примеры

--- js/ydkjs/async/async.js
+++ js/ydkjs/async/async.js
@@ -343,10 +343,9 @@ console.log(promise3);
 console.log(promise4);
 console.log(promise5); */
 
-
 // @ Пример 10 -  что выведет и почему?
 
-var p = new Promise( function(resolve,reject){
+/* var p = new Promise( function(resolve,reject){
   foo.bar();  // `foo` не определено, ошибка!
   resolve( 42 );  // в эту точку управление не передается :(
 } );
@@ -358,16 +357,253 @@ p.then(
       // здесь `err` будет объектом исключения `TypeError`
       // из строки `foo.bar()`.
   }
-);
+); */
+
+// @ Мой пример 10 -  что выведет и почему?
+
+/* let a = 0;
+
+const d = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(5);
+    console.log(a);
+  }, 1);
+}).then((res) => {
+  console.log(a);
+});
+
+setTimeout(function () {
+  a = 1;
+  console.log(a);
+}, 0);
+
+console.log(a);
+
+const c = Promise.resolve(42).then((res) => {
+  console.log(a);
+  a = res;
+  console.log(a);
+});
+
+console.log(a);
+console.log(d); */
+
+// @ Мой пример 11 -  что выведет и почему?
+
+/* const foo = () => {
+  return 111
+};
+const v = Promise.resolve(foo()).then(res=>{
+  console.log(res);
+  // return Promise.resolve(res)
+}).then(res=>{
+  console.log(res);
+})
+const v2 = Promise.resolve(foo())
+console.log(v);
+console.log(5)
+console.log(v2); */
+
+// @ Мой пример 12 -  что выведет и почему?
+
+/*  const foo = () => {
+  return 111
+};
+const a = foo()
+const b = Promise.resolve(foo())
+const d = Promise.resolve(foo()).then(res=>{
+  console.log(res);
+})
+
+console.log(b);
+console.log(a);
+console.log(d);
+ */
+
+// @ Мой пример 13 -  что выведет и почему? Демонстрация сцепления then
+/* var p = Promise.resolve( 21 );
+var p2 = p.then( function(v){
+    console.log( v );   // 21
+    // выполнение `p2` со значением `42`
+     return v * 2;
+} );
+console.log(p2);
+// сцепление `p2`
+p2.then( function(v){
+    console.log( v );
+} ); */
+
+// @ Мой пример 14 -  что выведет и почему? Введение setTimeout, так можно создавать задержку для следующего шага
+
+/* var p = Promise.resolve( 21 );
+p.then( function(v){
+    console.log( v );   // 21
+    // создать обещание для возвращения
+    return new Promise( function(resolve,reject){
+        // ввести асинхронность!
+        setTimeout( function(){
+            // выполнить со значением `42`
+            resolve( v * 2 );
+        }, 100 );
+} ); })
+.then( function(v){
+    // выполняется после 100-миллисекундной задержки
+     // на предыдущем шаге
+    console.log( v );   // 42
+} );
+ */
+
+// @ Пример 15 -  что выведет и почему?
+
+/* function delay(time) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(resolve, time);
+  });
+}
+delay(100) // шаг 1
+  .then(function STEP2() {
+    console.log("step 2 (after 100ms)");
+    return delay(200);
+  })
+  .then(function STEP3() {
+    console.log("step 3 (after another 200ms)");
+  })
+  .then(function STEP4() {
+    console.log("step 4 (next Job)");
+    return delay(50);
+  })
+  .then(function STEP5() {
+    console.log("step 5 (after another 50ms)");
+  }); */
+
+// @ пример 16 -  что выведет и почему?
+
+/* // шаг 1:
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  // шаг 2:
+  .then(function (response1) {
+    foo.bar(); // undefined, ошибка!
+    // сюда управление не передается
+    return fetch("https://jsonplaceholder.typicode.com/todos/2");
+  })
+  // шаг 3:
+  .then(
+    function fulfilled(response2) {
+      // сюда управление не передается
+    },
+    // обработчик отказа для перехвата ошибки
+    function rejected(err) {
+      console.log(err);
+      // ошибка `TypeError` из `foo.bar()`
+      return 42;
+    }
+  )
+  // шаг 4:
+  .then(function (msg) {
+    console.log(msg);
+  }); */
+
+/* 
+  
+  Когда на шаге 2 происходит ошибка, обработчик отказа на шаге 3 перехватывает его. Возвращаемое значение от этого обработчика отказа (42 в данном фрагменте), если оно есть, выполняет обе- щание для следующего шага (4), так что цепочка снова находится в состоянии выполнения.
+  
+
+  У паттерна then(null,function(err){ .. }), который обра- батывает только отказы, но пропускает выполнения, существу- ет сокращенная запись в API: catch(function(err){ .. }) . Конструкция catch(..) будет более полно рассмотрена в сле- дующем разделе.
+  */
+/* 
+  
+  Вызов delay(200) создает обещание, которое будет выполнено через 200 мс.
+  Сконструированная нами цепочка обещаний не только обеспечи- вает управление программной логикой, выражающей многошаго- вую асинхронную последовательность, но и работает как канал сообщений для передачи сообщений от шага к шагу.
+  */
+
+// @ Пример 17 -  что выведет и почему? Обработка ошибок
+
+/* function foo() {
+  setTimeout(function () {
+    baz.bar();
+  }, 100);
+}
+try {
+  foo();
+  // позднее выдает глобальную ошибку из `baz.bar()`
+} catch (err) {
+  // управление сюда не передается
+} */
+
+/* 
+Самая естественная форма обработки ошибок для большинства разработчиков — синхронная конструкция try..catch.
+Если сама функция baz.bar() является асин- хронно завершаемой, то никакие асинхронные ошибки в ней перехватываться не будут.
+*/
+
+// @ Мой пример 18 -  что выведет и почему?
+
+/* document.addEventListener("click", ()=>{
+  console.log(777);
+})
+
+setTimeout(() => {
+  console.log(333);
+}, 3);
+
+setTimeout(() => {
+  console.log(222);
+}, 0);
+
+new Promise((resolve, reject) => {
+  console.log(666);
+  setTimeout(() => {
+    resolve(555);
+  }, 0);
+}).then((res) => {
+  console.log(res);
+});
+
+new Promise((resolve, reject) => {
+  resolve(444);
+})
+  .then((res) => {
+    console.log(res);
+    return res + 1;
+  })
+  .then((res) => {
+    console.log(res);
+    return res;
+  })
+  .finally((res) => {
+    console.log(res);
+  });
+
+console.log(111);
 
 
+let event = new MouseEvent("click", {
+  bubbles: true,
+  cancelable: true,
+  clientX: 100,
+  clientY: 100
+});
+document.dispatchEvent(event) */
+
+// § Факультатив. XMLHttpReques и Fetch
+//
+/* 
+
+XMLHttpRequest - встроенный в браузер объект, который дает возможность делать запросы к серверу без перезагрузки страницы. Не смотря на
+наличие XML он может работать с любыми типами данных. Более современный аналог метод fetch.
+
+
+Если функция возвращает Promise, тогда ей пишем async. await - значит, что на следующую строчку перейдем только когда ответ будет получен.
+*/
+
+// § Ссылки
+// ? https://blog.greenroots.info/task-queue-and-job-queue-deep-dive-into-javascript-event-loop-model - про таски
+
+// ? онлайн демонстрация цикла событий.
+// ? http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
+
+// console.log(b);
 // const todos = fetch("https://jsonplaceholder.typicode.com/todos/1");
 // .then(response => response.json())
 // .then(json => console.log(json))
 
 // console.log(todos);
-
-// § Ссылки
-// ? https://blog.greenroots.info/task-queue-and-job-queue-deep-dive-into-javascript-event-loop-model
-
-   console.log(history.push("/123"));
