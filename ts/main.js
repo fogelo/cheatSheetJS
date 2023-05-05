17ccc8419afde18aa6e8d8a608c5dca852aedc2a
fogelo <fogelo@yandex.ru>
2023-05-05 16:55:11 +0300

    Структурная типизация

--- ts/main.js
+++ ts/main.js
@@ -1,8 +1,27 @@
-var str = "not a number";
-var number1 = str;
-var number2 = str;
-var obj = { str: "hello", num: 42 };
-var city = 'new york city';
-console.log(city.toUppercase());
-var bbb = {};
+var city = "new york city";
+function add(a, b) {
+    return a + b;
+}
+function calculateLength(v) {
+    return Math.sqrt(v.x * v.x + v.y * v.y);
+}
+var v = { x: 3, y: 4, name: "Zee" };
+calculateLength(v);
+var age;
+age = "12";
+age = "12";
+age = 15;
+age = {};
+var obj1 = { str: "hello", num: 42 };
+obj.num;
+var obj2 = { str: "hello", num: 42 };
+obj2.str;
+if (typeof p === 'object') { }
+var aaa = fetch("asdf");
+var ps = {
+    name: 'Alan Turing',
+    birth: new Date('1912/06/23'),
+    death: new Date('1954/06/07'),
+    xxx: "adfasdf"
+};
 //# sourceMappingURL=main.js.map
